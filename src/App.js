4b714569c7597503348c50461f0e62cf8022f34a
Sathursanak <132430609+Sathursanak@users.noreply.github.com>
2025-01-22 19:55:13 +0530

    taskmanager

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Tasks from "./components/Tasks";
+import Update from "./components/Update";
+import Add from "./components/Add";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="h-full sm:p-10 lg:p-20 bg-teal-500">
+      <BrowserRouter>
+      <Routes>
+        <Route path = "/" element= {<Tasks/>}/>
+        <Route path = "/add" element= {<Add/>}/>
+        <Route path = "/update/:id" element= {<Update/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
